0e1450aafe95604e2921a700f5d74b2cd7cb389b
aribilgiogr <aribilgiogr@gmail.com>
2026-06-10 21:10:41 +0300

    Update  03 10.06.2026

---
+++ Core/Abstracts/IServices/IStockService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Concretes.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Abstracts.IServices
+{
+    public interface IProductService
+    {
+        Task<ProductDto> GetAsync(int id);
+        Task<IEnumerable<ProductDto>> GetAllAsync();
+        Task<IEnumerable<ProductDto>> GetAllByCategory(int categoryId);
+        Task<IEnumerable<ProductDto>> GetAllByBrand(int brandId);
+    }
+}
---
+++ Core/Concretes/DTOs/ProductDto.cs
@@ -0,0 +1,26 @@
+﻿namespace Core.Concretes.DTOs
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = null!;
+        public int BrandId { get; set; }
+        public string BrandName { get; set; } = null!;
+        public int Quantity { get; set; }
+        public DateTime LastUpdatedTime { get; set; }
+    }
+
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+
+    public class BrandDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
