dacbf36117ed575a34bfe475142788f06f750e11
aribilgiogr <aribilgiogr@gmail.com>
2026-06-22 21:37:19 +0300

    Update 02 22.06.2026

---
+++ Business/Services/BrandService.cs
@@ -0,0 +1,69 @@
+﻿using Core.Abstracts;
+using Core.Abstracts.IServices;
+using Core.Concretes.DTOs;
+using Core.Concretes.Entities;
+
+namespace Business.Services
+{
+    public class BrandService : IBrandService
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public BrandService(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task CreateAsync(string name)
+        {
+            var brand = new Brand { Name = name };
+            await unitOfWork.Brands.CreateAsync(brand);
+            await unitOfWork.CommitAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var brand = await unitOfWork.Brands.FindOneAsync(id);
+            if (brand != null)
+            {
+                await unitOfWork.Brands.DeleteAsync(brand);
+                await unitOfWork.CommitAsync();
+            }
+        }
+
+        public async Task<IEnumerable<BrandDto>> GetAllAsync()
+        {
+            var brands = await unitOfWork.Brands.FindManyAsync();
+            return from b in brands.ToList()
+                   select new BrandDto
+                   {
+                       Id = b.Id,
+                       Name = b.Name
+                   };
+        }
+
+        public async Task<BrandDto> GetAsync(int id)
+        {
+            var brand = await unitOfWork.Brands.FindOneAsync(id);
+            if (brand != null)
+            {
+                return new BrandDto
+                {
+                    Id = brand.Id,
+                    Name = brand.Name
+                };
+            }
+            return null;
+        }
+
+        public async Task UpdateAsync(BrandDto updatedBrand)
+        {
+            var brand = await unitOfWork.Brands.FindOneAsync(updatedBrand.Id);
+            if (brand != null)
+            {
+                await unitOfWork.Brands.UpdateAsync(brand);
+                await unitOfWork.CommitAsync();
+            }
+        }
+    }
+}
---
+++ Business/Services/CategoryService.cs
@@ -0,0 +1,69 @@
+﻿using Core.Abstracts;
+using Core.Abstracts.IServices;
+using Core.Concretes.DTOs;
+using Core.Concretes.Entities;
+
+namespace Business.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public CategoryService(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task CreateAsync(string name)
+        {
+            var category = new Category { Name = name };
+            await unitOfWork.Categories.CreateAsync(category);
+            await unitOfWork.CommitAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var category = await unitOfWork.Categories.FindOneAsync(id);
+            if (category != null)
+            {
+                await unitOfWork.Categories.DeleteAsync(category);
+                await unitOfWork.CommitAsync();
+            }
+        }
+
+        public async Task<IEnumerable<CategoryDto>> GetAllAsync()
+        {
+            var categories = await unitOfWork.Categories.FindManyAsync();
+            return from c in categories.ToList()
+                   select new CategoryDto
+                   {
+                       Id = c.Id,
+                       Name = c.Name
+                   };
+        }
+
+        public async Task<CategoryDto?> GetAsync(int id)
+        {
+            var category = await unitOfWork.Categories.FindOneAsync(id);
+            if (category != null)
+            {
+                return new CategoryDto
+                {
+                    Id = category.Id,
+                    Name = category.Name
+                };
+            }
+            return null;
+        }
+
+        public async Task UpdateAsync(CategoryDto updatedCategory)
+        {
+            var category = await unitOfWork.Categories.FindOneAsync(updatedCategory.Id);
+            if (category != null)
+            {
+                category.Name = updatedCategory.Name;
+                await unitOfWork.Categories.UpdateAsync(category);
+            }
+        }
+    }
+}
